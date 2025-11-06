084393eba3ec114624738da883ad89de4d679729
hatonlee <hatonen.leevi@gmail.com>
2025-11-06 11:02:56 +0200

    test: added tests for maksukortti [100%]

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -5,5 +5,34 @@ class TestMaksukortti(unittest.TestCase):
     def setUp(self):
         self.maksukortti = Maksukortti(1000)
 
-    def test_luotu_kortti_on_olemassa(self):
+    def test_luonti_onnistuu(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_alku_saldo_oikein(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_rahan_lataus_toimii(self):
+        self.maksukortti.lataa_rahaa(2500)
+
+        self.assertEqual(self.maksukortti.saldo_euroissa(), 35.0)
+
+    def test_negatiivinen_rahan_lataus_ei_muuta_saldoa(self):
+        self.maksukortti.lataa_rahaa(-500)
+
+        self.assertEqual(self.maksukortti.saldo_euroissa(), 10.0)
+
+    def test_rahan_ottaminen_toimii(self):
+        self.maksukortti.ota_rahaa(500)
+
+        self.assertEqual(self.maksukortti.saldo_euroissa(), 5.0)
+
+    def test_rahan_ottaminen_liikaa_ei_muuta_saldoa(self):
+        self.maksukortti.ota_rahaa(1500)
+
+        self.assertEqual(self.maksukortti.saldo_euroissa(), 10.0)
+
+    def test_rahan_ottaminen_palauttaa_true(self):
+        self.assertTrue(self.maksukortti.ota_rahaa(500))
+
+    def test_rahan_ottaminen_liikaa_palauttaa_false(self):
+        self.assertFalse(self.maksukortti.ota_rahaa(1500))
